4c8cc2eb34c4944ddc73c52f1b01a2c2c0438649
Usuari ASO <aso@numenor>
2013-10-21 19:52:13 +0200

    hola

--- Script.sh
+++ Script.sh
@@ -9,25 +9,25 @@ if [ $# -ne 0 ]; then
 	    echo $usage; exit 1
 	fi
     else
-	echo $usage; exit 1
+		echo $usage; exit 1
     fi
 fi
 
 for user in `cut -d: -f1 /etc/passwd`; do
     home=`cat /etc/passwd | grep "^$user\>" | cut -d: -f6`
     if [ -d $home ]; then
-	num_fich=`find $home -type f -user $user | wc -l`
+		num_fich=`find $home -type f -user $user | wc -l`
     else
-	num_fich=0
+		num_fich=0
     fi
     if [ $num_fich -eq 0 ] ; then
-	if [ $p -eq 1 ]; then
-	    user_proc=`ps -u $user --no-headers|wc -l`
-	    if [ $user_proc -eq 0 ]; then
-		echo "$user"
-	    fi
-	else
-	    echo "$user"
-	fi
+		if [ $p -eq 1 ]; then
+		    user_proc=`ps -u $user --no-headers|wc -l`
+		    if [ $user_proc -eq 0 ]; then
+				echo "$user"
+		    fi
+		else
+		    echo "$user"
+		fi
     fi 
 done
