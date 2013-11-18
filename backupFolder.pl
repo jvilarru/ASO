ecd9d4e3b26466ec9b2ef96d0de3d6ce0e4cda72
Usuari ASO <aso@numenor>
2013-11-18 19:49:02 +0100

    commituyh

--- backupFolder.pl
+++ backupFolder.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
-$sais = @ARGV;
-if($sais < 1 || $sais > 3) {
-	print "Usage: perl backupFolder.pl Source ExcludeFile lastBackup";
+$mida = @ARGV;
+if($mida < 1 || $mida > 3) {
+	print "Usage: perl backupFolder.pl Source [lastBackup] [ExcludeFile]";
 	exit -1;
 }
 $date = `date +"%Y-%m-%d-%H-%M-%S"`;
@@ -9,16 +9,20 @@ chomp($date);
 $arxiu = $ARGV[0];
 $arxiu =~ s/\//_/;
 $nom = "";
-if($sais <= 2){
+if($mida < 2){
 	$nom = "backup-$arxiu-nivell0-$date";
 }
 else{
 	$nom = "backup-$arxiu-nivell1-$date";
 }
-if($sais == 3){
-	`tar cf "$nom.tar" --newer $ARGV[2] -X $ARGV[1] $ARGV[0]`;
+
+if($mida == 3){
+	`tar cf "$nom.tar" --newer $ARGV[1] -X $ARGV[2] $ARGV[0]`;
+}
+elsif($mida == 2){
+	`tar cf "$nom.tar" --newer $ARGV[1] $ARGV[0]`;
 }
 else{
-	`tar cf "$nom.tar" --newer $ARGV[2] $ARGV[0]`;
+	`tar cf "$nom.tar" $ARGV[0]`;
 }
 `md5sum "$nom.tar" > "$nom.asc"`
